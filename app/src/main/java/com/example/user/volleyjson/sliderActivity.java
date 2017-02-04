7eac23fafd1247b9c3260809234bef6aa57ec619
ashchalstha <ashchal93@gmail.com>
2017-02-04 15:26:25 +0545

    sliderwindow and upbutton as home2

---
+++ app/src/main/java/com/example/user/volleyjson/sliderActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.volleyjson;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class sliderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slider);
+    }
+}
