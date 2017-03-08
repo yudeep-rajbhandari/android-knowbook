c3ff16b652dab5f5c2ebc5face72f579ddfe7424
ashchalstha <ashchal93@gmail.com>
2017-03-08 15:10:05 +0545

    tyei

---
+++ app/src/main/java/com/example/user/volleyjson/navigation_drawer.java
@@ -0,0 +1,18 @@
+package com.example.user.volleyjson;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ashchal on 2/25/17.
+ */
+
+public class navigation_drawer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.menu.activity_navigation_drawer);
+    }
+}
